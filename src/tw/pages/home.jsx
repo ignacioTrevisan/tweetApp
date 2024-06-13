6fa20fa21cea067fb0d5721f813c35be2727efbf
Ignacio Trevisan <nachotizii988@gmail.com>
2024-06-13 17:08:56 -0300

    se borraron importaciones innecesarias

--- src/tw/pages/home.jsx
+++ src/tw/pages/home.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { SideBar } from '../components/sideBar'
+
 import { SocialArea } from './socialArea'
 import { LoadTrendingsAndTweets } from '../../hooks/loadTrendingsAndTweets'
 import { useSelector } from 'react-redux'
 import { LoadPage } from '../../ui/loadPage'
 import { Box, CssBaseline, Grid } from '@mui/material'
-import { Navbar } from '../../ui/navbar'
 import { NavbarDos } from '../../ui/navbarDos'
 import { SideBarDos } from '../components/sideBarDos'
 
@@ -23,8 +22,7 @@ export const Home = () => {
     return (
         <>
             <CssBaseline />
-            {/* <SideBar Display={Display} setDisplay={setDisplay} /> */}
-            {/* <Navbar Display={Display} setDisplay={setDisplay} /> */}
+
             <NavbarDos Display={Display} setDisplay={setDisplay} />
             <SocialArea Display={Display} setDisplay={setDisplay} />
             <SideBarDos Display={Display} setDisplay={setDisplay} />
